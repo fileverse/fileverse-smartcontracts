3b121865e3f50e57f7e7cc6835708055c673fc90
Vijay Krishnavanshi <vijaykrishnavanshi@gmail.com>
2022-03-23 14:48:14 +0530

    Addeed ERC721 Smart Contract

---
+++ contracts/FileverseNFT.sol
@@ -0,0 +1,39 @@
+// contracts/FileverseNFT.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract FileverseNFT is ERC721 {
+    uint256 private _currentTokenId = 0;//Token ID here will start from 1
+
+    constructor(
+        string memory _name,
+        string memory _symbol
+    ) ERC721(_name, _symbol) {}
+
+    /**
+     * @dev Mints a token to an address with a tokenURI.
+     * @param _to address of the future owner of the token
+     */
+    function mintTo(address _to) public {
+        uint256 newTokenId = _getNextTokenId();
+        _mint(_to, newTokenId);
+        _incrementTokenId();
+    }
+
+    /**
+     * @dev calculates the next token ID based on value of _currentTokenId
+     * @return uint256 for the next token ID
+     */
+    function _getNextTokenId() private view returns (uint256) {
+        return _currentTokenId+1;
+    }
+
+    /**
+     * @dev increments the value of _currentTokenId
+     */
+    function _incrementTokenId() private {
+        _currentTokenId++;
+    }
+}
