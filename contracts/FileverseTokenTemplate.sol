39a714407365440422b57c6aa0f215332896ac0f
Vijay Krishnavanshi <vijaykrishnavanshi@gmail.com>
2022-04-21 11:32:01 +0530

    Updated Fileverse Token Template

--- contracts/FileverseTokenTemplate.sol
+++ contracts/FileverseTokenTemplate.sol
@@ -3,32 +3,32 @@ pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-/// @custom:security-contact security@fileverse.io
-contract FileverseTokenTemplate is ERC721, ERC721Enumerable, ERC721URIStorage, Pausable, AccessControl, ERC721Burnable {
+/// @custom:security-contact security@adaptiv.me
+contract ProfileNFT is ERC721, ERC721Enumerable, Pausable, AccessControl, ERC721Burnable {
     using Counters for Counters.Counter;
 
-    string public _baseUri = "";
     bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     Counters.Counter private _tokenIdCounter;
+
+    string public baseUri = "https://profiles.adaptiv.me/token/";
 
     constructor(
         string memory name,
         string memory symbol,
         address ownerAddress,
-        string memory baseUri
+        string memory newBaseUri
     ) ERC721(name, symbol) {
         require(
             ownerAddress != address(0),
             "ownerAddress cannot be zero"
         );
-        _baseUri = baseUri;
+        baseUri = newBaseUri;
         _grantRole(DEFAULT_ADMIN_ROLE, ownerAddress);
         _grantRole(PAUSER_ROLE, ownerAddress);
         _grantRole(MINTER_ROLE, ownerAddress);
@@ -37,7 +37,12 @@ contract FileverseTokenTemplate is ERC721, ERC721Enumerable, ERC721URIStorage, P
     }
 
     function _baseURI() internal view override returns (string memory) {
-        return _baseUri;
+        return baseUri;
+    }
+
+    function setBaseURI(string memory newBaseUri) external onlyRole(MINTER_ROLE) returns (string memory) {
+        baseUri = newBaseUri;
+        return baseUri;
     }
 
     function pause() public onlyRole(PAUSER_ROLE) {
@@ -70,21 +75,6 @@ contract FileverseTokenTemplate is ERC721, ERC721Enumerable, ERC721URIStorage, P
         override(ERC721, ERC721Enumerable)
     {
         super._beforeTokenTransfer(from, to, tokenId);
-    }
-
-    // The following functions are overrides required by Solidity.
-
-    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
-        super._burn(tokenId);
-    }
-
-    function tokenURI(uint256 tokenId)
-        public
-        view
-        override(ERC721, ERC721URIStorage)
-        returns (string memory)
-    {
-        return super.tokenURI(tokenId);
     }
 
     function supportsInterface(bytes4 interfaceId)
