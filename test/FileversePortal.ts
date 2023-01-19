d370cd5256690e5623ca90fbfeea6cbf88fd4b3b
Vijay Krishnavanshi <vijaykrishnavanshi@gmail.com>
2023-01-19 13:36:21 +0530

    test: added collaborator cases

--- test/FileversePortal.ts
+++ test/FileversePortal.ts
@@ -126,3 +126,112 @@ describe("Fileverse Portal: Owner", function () {
       .withArgs(0, ipfsHash, ipfsHash, ipfsHash, owner.address);
   });
 });
+
+describe("Fileverse Portal: Collaborator", function () {
+  async function deployPortalFixtureCollaborator() {
+    const [owner, addr1, addr2] = await ethers.getSigners();
+    const FileversePortal = await ethers.getContractFactory("FileversePortal");
+    const metadataIPFSHash = "QmWSa5j5DbAfHvALWhrBgrcEkt5PAPVKLzcjuCAE8szQp5";
+    const ownerViewDid =
+      "did:key:z6MkkiKsFrxyb6mDd6RaWjDuuBs84T8vFtPgCds7jEC9bPbo";
+    const ownerEditDid =
+      "did:key:z6MkjeNxGFLaSrTTRnQbDcfXytYb8wAZiY1yy1X2g678xuYD";
+    const addr1ViewDid =
+      "did:key:z6MkkiKsFrxyb6mDd6RaWjDuuBs84T8vFtPgCds7jEC9bPbo";
+    const addr1EditDid =
+      "did:key:z6MkjeNxGFLaSrTTRnQbDcfXytYb8wAZiY1yy1X2g678xuYD";
+
+    const trustedForwarder = "0x7EF22F49a2aE4a2E7c20369E6F7E5C9f94238141";
+
+    const fileversePortal = await FileversePortal.deploy(
+      metadataIPFSHash,
+      ownerViewDid,
+      ownerEditDid,
+      owner.address,
+      trustedForwarder
+    );
+    await fileversePortal.deployed();
+    await fileversePortal.addCollaborator(addr1.address);
+    // Fixtures can return anything you consider useful for your tests
+    return {
+      fileversePortal,
+      FileversePortal,
+      trustedForwarder,
+      metadataIPFSHash,
+      ownerViewDid,
+      ownerEditDid,
+      addr1ViewDid,
+      addr1EditDid,
+      owner,
+      addr1,
+      addr2,
+      AddressOne: "0x0000000000000000000000000000000000000001",
+      ipfsHash: "QmWSa5j5DbAfHvALWhrBgrcEkt5PAPVKLzcjuCAE8szQp6",
+    };
+  }
+
+  it("should be able to register self as member", async function () {
+    const { fileversePortal, addr1EditDid, addr1ViewDid, addr1 } =
+      await loadFixture(deployPortalFixtureCollaborator);
+    await expect(
+      fileversePortal
+        .connect(addr1)
+        .registerSelfToMember(addr1ViewDid, addr1EditDid)
+    )
+      .to.emit(fileversePortal, "RegisteredMember")
+      .withArgs(addr1.address);
+    expect(await fileversePortal.getMemberCount()).to.equal(2);
+  });
+
+  it("should be able to register and remove self as member", async function () {
+    const { fileversePortal, addr1EditDid, addr1ViewDid, addr1 } =
+      await loadFixture(deployPortalFixtureCollaborator);
+    await expect(
+      fileversePortal
+        .connect(addr1)
+        .registerSelfToMember(addr1ViewDid, addr1EditDid)
+    )
+      .to.emit(fileversePortal, "RegisteredMember")
+      .withArgs(addr1.address);
+    expect(await fileversePortal.getMemberCount()).to.equal(2);
+    await expect(fileversePortal.connect(addr1).removeSelfFromMember())
+      .to.emit(fileversePortal, "RemovedMember")
+      .withArgs(addr1.address);
+    expect(await fileversePortal.getMemberCount()).to.equal(1);
+  });
+
+  it("should be able to add file", async function () {
+    const { fileversePortal, ipfsHash, addr1 } = await loadFixture(
+      deployPortalFixtureCollaborator
+    );
+    expect(
+      await fileversePortal
+        .connect(addr1)
+        .addFile(ipfsHash, ipfsHash, ipfsHash, 1, 0)
+    )
+      .to.emit(fileversePortal, "AddedFile")
+      .withArgs(0, ipfsHash, ipfsHash, ipfsHash, addr1.address);
+    expect(await fileversePortal.getFileCount()).to.equal(1);
+  });
+
+  it("should be able to edit file", async function () {
+    const { fileversePortal, ipfsHash, addr1 } = await loadFixture(
+      deployPortalFixtureCollaborator
+    );
+    expect(
+      await fileversePortal
+        .connect(addr1)
+        .addFile(ipfsHash, ipfsHash, ipfsHash, 1, 0)
+    )
+      .to.emit(fileversePortal, "AddedFile")
+      .withArgs(0, ipfsHash, ipfsHash, ipfsHash, addr1.address);
+    expect(await fileversePortal.getFileCount()).to.equal(1);
+    expect(
+      await fileversePortal
+        .connect(addr1)
+        .editFile(0, ipfsHash, ipfsHash, ipfsHash, 1, 0)
+    )
+      .to.emit(fileversePortal, "EditedFile")
+      .withArgs(0, ipfsHash, ipfsHash, ipfsHash, addr1.address);
+  });
+});
